49bf603e3c9a0378b5412e9eb4ecb2f8a6a88042
CB <23136@rosmini.school.nz>
2026-04-24 12:00:33 +1200

    Add remove cart button
    Add remove cart button

--- flaskhtml.py
+++ flaskhtml.py
@@ -128,6 +128,23 @@ def remove_from_cart(item_id):
     session.modified = True
     return redirect(url_for('view_cart', globalProducts=products_globals))
 #end
+@app.route('/cart_delete/<int:item_id>')
+def remove_all_from_cart(item_id):
+    cart = session.get('cart', {})
+    string_id = str(item_id)
+
+    if string_id in cart:
+        if cart[string_id] > 1:
+            cart[string_id] = -1
+        else:
+            cart.pop(string_id)
+        #end
+    #end
+            
+    session['cart'] = cart
+    session.modified = True
+    return redirect(url_for('view_cart', globalProducts=products_globals))
+#end
 
 @app.route('/cart')
 def view_cart():
